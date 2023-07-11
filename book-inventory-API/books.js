860e4b19bc965fdbcf9f1449df1758de83cce8ef
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-07-11 15:13:30 -0400

    postgreSQL operating

---
+++ book-inventory-API/books.js
@@ -0,0 +1,21 @@
+const books = [
+    {
+        id: 1,
+        title: "The Life and Death of Great American Cities",
+        author: "Jane Jacobs",
+        pages: 458,
+        yearPublished: 1961,
+        genre: "Non-Fiction",
+        status: 1
+    },
+    {
+        id: 2,
+        title: "Conjectures of a Guilty Bystander",
+        author: "Thomas Merton",
+        pages: 400,
+        yearPublished: 1966,
+        genre: "Auto-Biography",
+        status: 2
+
+    }
+]
---
+++ book-inventory-API/database.js
@@ -0,0 +1,43 @@
+const { Pool } = require('pg');
+require("dotenv").config();
+
+// console.log(process.env.DB_HOST);
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS book_inventory (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    pages INTEGER,
+    yearPublished INTEGER,
+    genre VARCHAR(100),
+    status INTEGER NOT NULL
+  );
+`;
+
+const createTable = async () => {
+    try {
+      await pool.query(createTableQuery);
+      console.log('Table created successfully');
+    } catch (err) {
+      console.error('Error executing query', err.stack);
+    }
+  };
+  
+
+  module.exports = {
+    query: (text, params, callback) => {
+      console.log("QUERY:", text, params || "");
+      return pool.query(text, params, callback);
+    },
+  };
+
+  createTable();
